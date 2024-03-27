d1c3ca509c99073bbf4765c94dc4f00e5dba85a0
BountifulGames <hunter.wright98@yahoo.com>
2024-03-26 21:00:48 -0400

    Levels Made, gamemanager code updated

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,23 +13,75 @@ public class GameManager : MonoBehaviour
     public TMP_Text livesText;
     public GameObject gameOver;
     public GameObject youWon;
-    public GameObject bricksPrefab;
+    [SerializeField] private List<GameObject> bricksPrefabs = new List<GameObject>();
     public GameObject paddle;
     private GameObject clonePaddle;
+    [SerializeField] private int sceneNum;
 
     private static GameManager _instance;
-    public static GameManager Instance { get { return _instance; } }
+    public static GameManager Instance 
+    { 
+        get 
+        { 
+            return _instance; 
+        } 
+    }
 
     void Awake()
     {
-        DontDestroyOnLoad(Instance);
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+
+        sceneNum = 0;
+    }
+
+    private void Start()
+    {
         Setup();
-        bricks = bricksPrefab.transform.childCount;
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            LoadNextScene();
+        }
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+
+        Debug.Log("OnSceneLoaded: " + scene.name);
+
+        Setup();
+
     }
     public void Setup()
     {
+        Time.timeScale = 1f;
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+            lives = 3;
+            livesText.text = "Lives: " + lives;
+        }
+
         clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity);
-        Instantiate(bricksPrefab, transform.position, Quaternion.identity);
+        Instantiate(bricksPrefabs[sceneNum], transform.position, Quaternion.identity);
+        bricks = bricksPrefabs[sceneNum].transform.childCount;
+
+        youWon.SetActive(false);
+        gameOver.SetActive(false);
+
     }
 
     void CheckGameOver()
@@ -38,19 +90,32 @@ public class GameManager : MonoBehaviour
         {
             youWon.SetActive(true);
             Time.timeScale = .25f;
-            Invoke("Reset", resetDelay);
+            if (sceneNum < 3)
+            {
+                lives++;
+            }
+            Invoke("LoadNextScene", resetDelay);
         }
         if (lives < 1)
         {
             gameOver.SetActive(true);
             Time.timeScale = .25f;
             Invoke("Reset", resetDelay);
+
         }
     }
     void Reset()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        sceneNum = 0;
+        SceneManager.LoadScene("Level1");
+    }
+
+    private void LoadNextScene()
+    {
+        sceneNum++;
+        sceneNum = Mathf.Clamp(sceneNum++, 0, 2);
+        SceneManager.LoadScene(sceneNum);
     }
     public void LoseLife()
     {
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -9,11 +9,11 @@ public class PaddleController : MonoBehaviour
 
     private void Start()
     {
-        paddleSpeed = 0.05f;
+        paddleSpeed = 100f;
     }
     void Update()
     {
-        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed * Time.deltaTime);
         playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);
         transform.position = playerPos;
     }
